473b201460331f9ef2886903671521121729283e
Mathis Lenormand <lenormandmathis@gmail.com>
2020-02-01 10:51:47 +0100

    merge of "develop" and "origin/develop"

---
+++ Assets/Scripts/Controller/ControllerTouch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTouch : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch finger1 = Input.GetTouch(0);
+
+            Debug.Log(finger1.deltaPosition);
+        }
+    }
+}
