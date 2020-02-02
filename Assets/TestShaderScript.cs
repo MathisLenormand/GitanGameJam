d4acdcb6139237671a66eaeb4be364ff38d82bf9
Lilian <l.vidal@student.isartdigital.com>
2020-02-02 09:08:13 +0100

    Add UI and TestShaderScript

---
+++ Assets/TestShaderScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestShaderScript : MonoBehaviour
+{
+    public Material organicmat;
+    public bool testamount;
+    public bool testblend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        organicmat = GetComponent<Renderer>().material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testamount == true)
+        {
+            organicmat.SetFloat("_Amount", 500);
+        }
+        else
+        {
+            organicmat.SetFloat("_Amount", 2);
+        }
+
+        if (testblend == true)
+        {
+            organicmat.SetFloat("_blend", 1);
+        }
+        else
+        {
+            organicmat.SetFloat("_blend", 0);
+        }
+    }
+}
